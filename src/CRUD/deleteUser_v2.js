d4cba16a6358247f343051f90d8155350be15028
SummerTale <101444274+SummerTale@users.noreply.github.com>
2023-12-03 13:03:34 -0600

    deleteUser_v2.js
    need to test this file

---
+++ src/CRUD/deleteUser_v2.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import axios from 'axios';
+
+const DeleteUser = ({ flaskUrl, userId }) => {
+  const handleDeleteUser = async () => {
+    try {
+      // Delete associated reviews
+      await axios.delete(`${flaskUrl}/reviews/user/${userId}`);
+      
+      // Delete associated favorites
+      await axios.delete(`${flaskUrl}/favorites/user/${userId}`);
+      
+      // Finally, delete the user
+      const response = await axios.delete(`${flaskUrl}/users/${userId}`);
+      
+      console.log(response.data.message); // Log the response message
+
+      // You can also update the UI as needed after successful deletion
+    } catch (error) {
+      console.error('Error deleting user:', error);
+      // Handle the error, e.g., show an error message to the user
+    }
+  };
+
+  return (
+    <form onSubmit={handleDeleteUser}>
+      <div>
+        <button type="submit">Delete User</button>
+      </div>
+  );
+};
+
+export default DeleteUser;
